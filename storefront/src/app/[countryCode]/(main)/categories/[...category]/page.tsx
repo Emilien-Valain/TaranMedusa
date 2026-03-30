9c27c623c3beba18a96eca8779af2bcd654ae228
Emilien Valain <emilien.valain@gmail.com>
2026-03-30 22:55:12 +0200

    fix: force dynamic pages

--- storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -1,11 +1,10 @@
 import { getCategoryByHandle, listCategories } from "@/lib/data/categories"
-import { listRegions } from "@/lib/data/regions"
 import CategoryTemplate from "@/modules/categories/templates"
 import { SortOptions } from "@/modules/store/components/refinement-list/sort-products"
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
 
-export const dynamicParams = true
+export const dynamic = "force-dynamic"
 
 type Props = {
   params: Promise<{ category: string[]; countryCode: string }>
@@ -37,34 +36,6 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   }
 }
 
-export async function generateStaticParams() {
-  try {
-    const countryCodes = await listRegions().then(
-      (regions) =>
-        regions
-          ?.map((r) => r.countries?.map((c) => c.iso_2))
-          .flat()
-          .filter(Boolean) as string[]
-    )
-
-    if (!countryCodes) {
-      return []
-    }
-
-    const categories = await listCategories()
-
-    return countryCodes
-      .map((countryCode) =>
-        categories.map((category) => ({
-          countryCode,
-          category: category.handle.split("/"),
-        }))
-      )
-      .flat()
-  } catch {
-    return []
-  }
-}
 
 export default async function CategoryPage(props: Props) {
   const searchParams = await props.searchParams
--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,31 +1,15 @@
-import { listRegions } from "@/lib/data/regions"
 import FeaturedProducts from "@/modules/home/components/featured-products"
 import Hero from "@/modules/home/components/hero"
 import SkeletonFeaturedProducts from "@/modules/skeletons/templates/skeleton-featured-products"
 import { Metadata } from "next"
 import { Suspense } from "react"
 
-export const dynamicParams = true
+export const dynamic = "force-dynamic"
 
 export const metadata: Metadata = {
   title: "Taran Industrie - Solutions professionnelles de nettoyage",
   description:
     "Taran Industrie - Définir les besoins, livrer les solutions ! Lingettes professionnelles et produits de nettoyage industriels.",
-}
-
-export async function generateStaticParams() {
-  try {
-    const countryCodes = await listRegions().then(
-      (regions) =>
-        regions
-          ?.map((r) => r.countries?.map((c) => c.iso_2))
-          .flat()
-          .filter(Boolean) as string[]
-    )
-    return countryCodes.map((countryCode) => ({ countryCode }))
-  } catch {
-    return []
-  }
 }
 
 export default async function Home(props: {
